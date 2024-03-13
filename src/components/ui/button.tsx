eb648c47a0a3508660202c5dd6da74a3d19ce8e4
Lucas Terr <terr@usc.edu>
2024-03-13 19:23:54 -0400

    update button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,15 +5,15 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  `inline-flex items-center justify-center whitespace-nowrap w-fit rounded-full text-sm font-medium ring-offset-normal-50 transition-all
+  `inline-flex items-center justify-center whitespace-nowrap w-fit rounded-full text-sm font-medium ring-offset-light transition-all
     duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-normal-950 focus-visible:ring-offset-2
     disabled:pointer-events-none disabled:opacity-50 relative`,
   {
     variants: {
       variant: {
-        secondary: "bg-transparent text-normal-50 outline outline-normal-50",
+        secondary: "bg-transparent text-light outline outline-light outline-px",
         primary:
-          "bg-gradient-primary hover:text-dark hover:brightness-125 text-normal-50",
+          "bg-gradient-primary hover:text-dark hover:brightness-125 text-light",
       },
       size: {
         default: "h-10 px-4 py-2",
