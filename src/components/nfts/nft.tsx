3df983940b46ca3bdc55e08fdc835916bb34bcf4
Lucas Terr <terr@usc.edu>
2024-04-13 23:46:28 +0900

    use router instead of link for nft click navigation

--- src/components/nfts/nft.tsx
+++ src/components/nfts/nft.tsx
@@ -1,22 +1,26 @@
 import { PotrMetadata } from "potr-common";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import Image from "next/image";
-import { cn } from "@/lib/utils";
 import Animated from "@/styles/transition";
-import Link from "next/link";
 import { Route } from "@/lib/routes";
+import { useRouter } from "next/navigation";
 
 type NftProps = {
   metadata: PotrMetadata;
 };
 
 export default function Nft({ metadata }: NftProps) {
+  const router = useRouter();
+
+  // href for the details page for this specific nft
+  const detailPageHref = `${Route.NFTS}/${metadata.unitName}`;
+
   const className = Animated(
-    "border-none bg-gradient-primary p-px hover:rotate-3 hover:scale-105"
+    "border-none bg-gradient-primary p-px hover:rotate-3 hover:scale-105 hover:cursor-pointer"
   );
 
   return (
-    <Card className={className}>
+    <Card className={className} onClick={() => router.push(detailPageHref)}>
       <CardHeader className="absolute bg-gradient-primary p-px h-fit inset-0 w-fit top-4 start-5 rounded-md flex items-center -skew-x-24">
         <CardTitle className="text-xs font-extrabold bg-dark rounded-md py-1 px-2">
           {metadata.name}
@@ -29,15 +33,14 @@ export default function Nft({ metadata }: NftProps) {
         </CardTitle>
       </CardHeader>
       <CardContent className="bg-dark p-1 rounded-lg">
-        <Link href={`${Route.NFTS}/${metadata.unitName}`}>
-          <Image
-            alt={metadata.name}
-            src={metadata.url}
-            width={240}
-            height={240}
-            className="rounded-lg"
-          />
-        </Link>
+        <Image
+          alt={metadata.name}
+          src={metadata.url}
+          loading="lazy"
+          width={240}
+          height={240}
+          className="rounded-lg"
+        />
       </CardContent>
     </Card>
   );
